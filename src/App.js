4f8766bfd117a343511e071dc14d322cc73b371b
rusty-reebs <rusty_reebs@tutanota.com>
2021-11-24 15:34:44 +0400

    initial config and file set up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
 
-function App() {
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDibCxXn_EsXjSIDfEsD2Dq1ZjZuiLhwtM",
+  authDomain: "wheres-waldo-82629.firebaseapp.com",
+  projectId: "wheres-waldo-82629",
+  storageBucket: "wheres-waldo-82629.appspot.com",
+  messagingSenderId: "1042527864001",
+  appId: "1:1042527864001:web:9b010bf07fabab106e0040",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const gameImage = require("./img/waldo-1.jpeg");
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={gameImage.default} alt="Not found" />
     </div>
   );
-}
+};
 
 export default App;
